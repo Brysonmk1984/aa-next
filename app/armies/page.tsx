b10ab5df388c4bbb49263364d17d6124df34609d
Bryson Kruk <brysonmk1984@gmail.com>
2024-06-09 07:25:08 -0600

    fixed some styles

--- app/armies/page.tsx
+++ app/armies/page.tsx
@@ -3,22 +3,23 @@ import { PageTemplate } from '@/components/PageTemplate.component';
 import TdBuyCell from '@/components/TdBuyCell';
 import { getArmies, getNationAndArmies, handleUserUpdateCheck } from '@/services';
 
-import { Nation, ResolvedUser, Army as ArmyType } from '@/types';
+import { Nation, ResolvedUser, Army as ArmyType, NationArmy } from '@/types';
 
 export default async function Army() {
   const session = await getAuth0Session();
 
   let armies: Array<ArmyType>;
-  let nationAndArmies: Nation;
+  let nation: Nation;
+  let nationArmies: NationArmy[];
 
   if (session) {
     const { id: userId } = await handleUserUpdateCheck(session.user);
 
     armies = await getArmies();
-    nationAndArmies = await getNationAndArmies(userId);
+    ({ nation, armies: nationArmies } = await getNationAndArmies(userId));
   }
 
-  if (!(session && nationAndArmies && armies)) {
+  if (!(session && nation && armies)) {
     return <em>Display the army cards here</em>;
   }
 
@@ -41,7 +42,7 @@ export default async function Army() {
               <td className="army-count">{army.count}</td>
               <td className="army-name">{army.name}</td>
               <td className="army-cost">ㆆ&nbsp;20,000</td>
-              {session && <TdBuyCell army={army} nationId={nationAndArmies.id} accessToken={session.accessToken} />}
+              {session && <TdBuyCell army={army} nationId={nation.id} accessToken={session.accessToken} />}
             </tr>
           ))}
         </tbody>
