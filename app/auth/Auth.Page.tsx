2a03ef37de15d22af7a5a86a8a9ed3065615174e
Bryson Kruk <brysonmk1984@gmail.com>
2024-08-13 07:56:35 -0600

    new auth page now handling two views

--- app/auth/Auth.Page.tsx
+++ app/auth/Auth.Page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ComponentType } from 'react';
+import { ComponentType, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 type Inputs = {
@@ -11,36 +11,87 @@ type Inputs = {
 interface Auth {}
 
 export const AuthPage: ComponentType<Auth> = ({}) => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState,
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log('DATA', data);
-
+  const [displaySignUpView, setDisplaySignUpView] = useState(false);
+  const { register, handleSubmit, watch, formState } = useForm<Inputs>();
+  const { register: signInRegister, handleSubmit: signInSubmit, formState: signInFormState } = useForm<Inputs>();
   console.log(watch('example')); // watch input value by passing the name of it
 
-  console.log({formState});
-  
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log('Log in Data', data);
 
-  return (
+  const onSignInSubmit: SubmitHandler<Inputs> = (data) => console.log('Sign Up Data', data);
+
+  const toggleView = () => {
+    setDisplaySignUpView((val) => !val);
+  };
+
+  return displaySignUpView ? (
+    <>
+      <h1 className="block text-center">Sign Up</h1>
+      <div className="w-2/3 mx-auto mt-8">
+        {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+        <form onSubmit={handleSubmit(onSignInSubmit)} className="flex flex-col items-left gap-3 text-center">
+          <label className="flex justify-end align-baseline items-end">
+            <span>Email</span>
+            {/* register your input into the hook by invoking the "register" function */}
+            <input className="w-full" defaultValue="test" {...signInRegister('example')} />
+          </label>
+
+          <label className="flex justify-end align-baseline items-end">
+            <span>Password</span>
+            {/* include validation with required or other standard HTML validation rules */}
+            <input className="w-full" type="password" {...signInRegister('exampleRequired', { required: true })} />
+          </label>
+          {/* errors will return when field validation fails  */}
+          {formState.errors.exampleRequired && <span>This field is required</span>}
+
+          <label className="flex justify-end align-baseline items-end">
+            <span className="text-nowrap">Confirm Password</span>
+            {/* include validation with required or other standard HTML validation rules */}
+            <input className=" w-full" type="password" {...signInRegister('exampleRequired', { required: true })} />
+          </label>
+          {/* errors will return when field validation fails  */}
+          {formState.errors.exampleRequired && <span>This field is required</span>}
+
+          <input className="btn btn-transparent mt-12" type="submit" />
+          <h2 className="mt-8 text-lg">
+            Or{' '}
+            <span className="link-style" onClick={toggleView}>
+              click here{' '}
+            </span>
+            to Log In
+          </h2>
+        </form>
+      </div>
+    </>
+  ) : (
     <>
       <h1 className="block text-center">Login</h1>
-      <div className="w-2/3 mx-auto">
-      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
-        {/* register your input into the hook by invoking the "register" function */}
-        <input className="mb-8  w-full" defaultValue="test" {...register('example')} placeholder="Enter Email" />
+      <div className="w-2/3 mx-auto mt-8">
+        {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-left gap-3 text-center">
+          <label className="flex justify-end align-baseline items-end">
+            <span>Email</span>
+            {/* register your input into the hook by invoking the "register" function */}
+            <input className="w-full" defaultValue="test" {...register('example')} />
+          </label>
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <input className="mb-8  w-full" {...register('exampleRequired', { required: true })}  placeholder="Enter Password" />
-        {/* errors will return when field validation fails  */}
-        {formState.errors.exampleRequired && <span>This field is required</span>}
+          <label className="flex justify-end align-baseline items-end">
+            <span>Password</span>
+            {/* include validation with required or other standard HTML validation rules */}
+            <input className=" w-full" {...register('exampleRequired', { required: true })} />
+          </label>
+          {/* errors will return when field validation fails  */}
+          {formState.errors.exampleRequired && <span>This field is required</span>}
 
-        <input className="btn btn-transparent" type="submit"  />
-        <h2 className='mt-8 text-lg'>Or <span className='link-style'>click here </span>to Sign Up</h2>
-      </form>
+          <input className="btn btn-transparent mt-12" type="submit" />
+          <h2 className="mt-8 text-lg">
+            Or{' '}
+            <span className="link-style" onClick={toggleView}>
+              click here{' '}
+            </span>
+            to Sign Up
+          </h2>
+        </form>
       </div>
     </>
   );
