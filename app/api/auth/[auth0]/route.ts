2b6545d2e01860ed969eb45e1045335d7981acaa
Bryson Kruk <brysonmk1984@gmail.com>
2024-08-24 08:42:04 -0600

    reenabled handleAuth

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,35 +1,33 @@
 // app/api/auth/[auth0]/route.js
-// import { AUTH_AUD } from '@/configs/environment.config';
-// import { errorType } from '@/utils';
+import { AUTH_AUD } from '@/configs/environment.config';
+import { errorType } from '@/utils';
 
-import { handleAuth } from '@auth0/nextjs-auth0';
-// import { headers } from 'next/headers';
-// import { NextApiRequest, NextApiResponse } from 'next/types';
+import { handleAuth, handleLogin, handleCallback } from '@auth0/nextjs-auth0';
+import { headers } from 'next/headers';
+import { NextApiRequest, NextApiResponse } from 'next/types';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  async login(req: NextApiRequest, res: NextApiResponse) {
+    const referrer = headers().get('referer');
+    const loggingInFromFoundingPage = referrer?.includes('/founding');
 
-// export const GET = handleAuth({
-//   async login(req: NextApiRequest, res: NextApiResponse) {
-//     const referrer = headers().get('referer');
-//     const loggingInFromFoundingPage = referrer?.includes('/founding');
+    console.log(referrer, loggingInFromFoundingPage);
 
-//     console.log(referrer, loggingInFromFoundingPage);
+    const returnTo = loggingInFromFoundingPage ? '/founding?authenticated=true' : (referrer ?? undefined);
 
-//     const returnTo = loggingInFromFoundingPage ? '/founding?authenticated=true' : (referrer ?? undefined);
+    try {
+      // Don't need to override the audience, it's already being passed from Auth0 server based on tenant
+      const handleLoginResponse = await handleLogin(req, res, {
+        // note: Auth_AUD needs to be the domain name
+        authorizationParams: { audience: AUTH_AUD },
+        returnTo,
+      });
 
-//     try {
-//       // Don't need to override the audience, it's already being passed from Auth0 server based on tenant
-//       const handleLoginResponse = await handleLogin(req, res, {
-//         // note: Auth_AUD needs to be the domain name
-//         authorizationParams: { audience: AUTH_AUD },
-//         returnTo,
-//       });
-
-//       return handleLoginResponse;
-//     } catch (e) {
-//       const error = errorType(e);
-//       console.error(error);
-//       res.status(500).end();
-//     }
-//   },
-// });
+      return handleLoginResponse;
+    } catch (e) {
+      const error = errorType(e);
+      console.error(error);
+      res.status(500).end();
+    }
+  },
+});
