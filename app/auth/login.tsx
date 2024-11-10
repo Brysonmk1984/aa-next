57aee3b4f78af7502e7e13bc13b32131d64bde42
Bryson Kruk <brysonmk1984@gmail.com>
2024-11-10 14:31:05 -0700

    wip

---
+++ app/auth/login.tsx
@@ -0,0 +1,25 @@
+import { auth0 } from '../../lib/auth0';
+
+export default async function Home() {
+  try {
+    const session = await auth0.getSession();
+    console.log({ session });
+
+    if (!session) {
+      return (
+        <main>
+          <a href="/auth/login?screen_hint=signup">Sign up</a>
+          <a href="/auth/login">Log in</a>
+        </main>
+      );
+    }
+
+    return (
+      <main>
+        <h1>Welcome, {session.user.name}!</h1>
+      </main>
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
