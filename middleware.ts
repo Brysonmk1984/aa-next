aeabcb15bf0128865645bf6a9a7c1224c469478d
Bryson Kruk <brysonmk1984@gmail.com>
2024-11-17 09:22:10 -0700

    wip

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { auth0 } from './lib/auth0';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+  console.log('in middle');
+
   return await auth0.middleware(request);
 }
 
